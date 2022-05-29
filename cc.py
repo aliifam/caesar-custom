83ef39e68fc95ec805a775995f5b48091313f2df
Aliif Arief <aliif.arief.m@gmail.com>
2022-05-29 18:27:24 +0700

    initial commit

---
+++ cc.py
@@ -0,0 +1,25 @@
+def caesar_encode(string, key):
+    lres = []
+    for i in range(1, len(string) + 1):
+        char = string[i - 1]
+        index = ord(char) + key + i
+        if index > 126:
+            index = (index - 126) + 33
+            lres.append(chr(index))
+        else:
+            lres.append(chr(index))
+    result = "".join(lres)
+    return result
+
+def caesar_decode(encoded, key):
+    lres = []
+    for i in range(1, len(encoded) + 1):
+        char = encoded[i - 1]
+        index = ord(char) - key - i
+        if index < 33:
+            index = (index + 126) - 33
+            lres.append(chr(index))
+        else:
+            lres.append(chr(index))
+    result = "".join(lres)
+    return result
---
+++ main.py
@@ -0,0 +1,11 @@
+from cc import caesar_encode, caesar_decode
+
+pw = "makangedebos"
+
+pw_encode = caesar_encode(pw, 4)
+
+print(pw_encode)
+
+pw_decode = caesar_decode(pw_encode, 4)
+
+print(pw_decode)
