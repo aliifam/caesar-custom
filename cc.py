4272c7d2b94f168cb7e8fd6cabff999d623abd07
fikrimilano <fikrimilano1@gmail.com>
2022-05-29 22:12:25 +0700

    Implement LinkedList

--- cc.py
+++ cc.py
@@ -1,29 +1,33 @@
+from LinkedList import LinkedList
+
 """
 character moved based on ASCII table
 """
 
+
 def caesar_encode(string, key):
-    lres = []
+    linked_list = LinkedList()
     for i in range(1, len(string) + 1):
         char = string[i - 1]
         index = ord(char) + key + i
         if index > 126:
             index = (index - 126) + 33
-            lres.append(chr(index))
+            linked_list.add_last(chr(index))
         else:
-            lres.append(chr(index))
-    result = "".join(lres)
+            linked_list.add_last(chr(index))
+    result = "".join(linked_list.as_array())
     return result
 
+
 def caesar_decode(encoded, key):
-    lres = []
+    linked_list = LinkedList()
     for i in range(1, len(encoded) + 1):
         char = encoded[i - 1]
         index = ord(char) - key - i
         if index < 33:
             index = (index + 126) - 33
-            lres.append(chr(index))
+            linked_list.add_last(chr(index))
         else:
-            lres.append(chr(index))
-    result = "".join(lres)
+            linked_list.add_last(chr(index))
+    result = "".join(linked_list.as_array())
     return result
